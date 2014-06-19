c378cd4fe9714ab2926d5fde17859fdc9b1e3365
incubus <lrs.incubus@googlemail.com>
2014-06-19 16:10:34 +0200

    forgot to declare a variable in the tests

--- UseUnaryOperator.cpp
+++ UseUnaryOperator.cpp
@@ -19,12 +19,14 @@ TEST( UseUnaryOperatorTest, PlusEqualToPrefixPlusPlus ) {
     // input for the transformation
     std::string cpp_input = 
 	"void fun() {\n"
+	"  int a = 0;\n"
         "  a += 1;\n"
 	"}\n"
     ;
     // the text that i expect to get after the transformation
     std::string cpp_output = 
 	"void fun() {\n"
+	"  int a = 0;\n"
 	"  ++a;\n"
 	"}\n"
     ;
